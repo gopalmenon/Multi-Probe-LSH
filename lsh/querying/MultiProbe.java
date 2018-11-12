17ac183fb836c219a94e67f65c9996317ac7c434
Gopal Menon <gopal.mnn@gmail.com>
2018-11-11 22:30:39 -0700

    Halfway coded perturbation. Still need to code perturbation validity check, shift and expand.

--- lsh/querying/MultiProbe.java
+++ lsh/querying/MultiProbe.java
@@ -1,8 +1,9 @@
 package lsh.querying;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Random;
 
 public class MultiProbe {
@@ -10,7 +11,7 @@ public class MultiProbe {
 	private int numberOfHashFunctions;
 	private double slotWidth;
 	Random randomNumberGenerator;
-	List<Double> randomPerturbations;
+	List<Double> randomDistancesToNextSlot;
 	List <Double> expectedValueNextSlotDistanceSquared;
 	List <Double> expectedValuePreviousSlotDistanceSquared;
 	
@@ -18,24 +19,23 @@ public class MultiProbe {
 		this.numberOfHashFunctions = numberOfHashFunctions;
 		this.slotWidth = slotWidth;
 		this.randomNumberGenerator = randomNumberGenerator;
-		this.randomPerturbations = new ArrayList<Double>(this.numberOfHashFunctions);
-		generateRandomPerturbations();
+		this.randomDistancesToNextSlot = new ArrayList<Double>(this.numberOfHashFunctions);
+		generateRandomDistancesToNextSlot();
 		this.expectedValueNextSlotDistanceSquared = new ArrayList<Double>(this.numberOfHashFunctions);
 		this.expectedValuePreviousSlotDistanceSquared = new ArrayList<Double>(this.numberOfHashFunctions);
 		findExpectedDistancesToAdjoiningSlots();
 	}
 	
-	private void generateRandomPerturbations() {
+	/**
+	 * Generate the joint distribution of of the Zj values for j = 1,..., M
+	 */
+	private void generateRandomDistancesToNextSlot() {
 		
 		double halfSlotWidth = this.slotWidth/2.0;
 		for (int hashFunctionCounter = 0; hashFunctionCounter < this.numberOfHashFunctions; ++hashFunctionCounter) {
-			this.randomPerturbations.add(Double.valueOf(this.randomNumberGenerator.nextDouble() * halfSlotWidth));
+			this.randomDistancesToNextSlot.add(Double.valueOf(this.randomNumberGenerator.nextDouble() * halfSlotWidth));
 		}
 
-	}
-	
-	public List<Double> getRandomPerturbations() {
-		return Collections.unmodifiableList(this.randomPerturbations);
 	}
 	
 	/**
@@ -49,12 +49,52 @@ public class MultiProbe {
 			computedValue = (hashFunctionCounter * (hashFunctionCounter + 1) * slotWidth * slotWidth) / (4.0 * (numberOfHashFunctions + 1) * (numberOfHashFunctions +2));
 			expectedValueNextSlotDistanceSquared.add(Double.valueOf(computedValue));
 			computedValue = slotWidth * slotWidth * (1.0 - 
-					((2.0 * numberOfHashFunctions + 1.0 - hashFunctionCounter) / (numberOfHashFunctions + 1.0)) - 
+					((2.0 * numberOfHashFunctions + 1.0 - hashFunctionCounter) / (numberOfHashFunctions + 1.0)) + 
 					(((2.0 * numberOfHashFunctions + 1.0 - hashFunctionCounter) * (2.0 * numberOfHashFunctions + 2.0 - hashFunctionCounter)) / (4.0 * (numberOfHashFunctions + 1.0) * (numberOfHashFunctions + 2.0))));
 			expectedValuePreviousSlotDistanceSquared.add(Double.valueOf(computedValue));
 		}
 		
 		
+	}
+	
+	private double getPerturbationScore(List<Integer> perturbedVector) {
+		return 0.0;
+	}
+	
+	private boolean isValidPerturbation(Perturbation candidatePerturbation) {
+		return true;
+	}
+	
+	private Perturbation shiftPerturbation(Perturbation inputPerturbation) {
+		return null;
+	}
+	
+	private Perturbation expandPerturbation(Perturbation inputPerturbation) {
+		return null;
+	}
+	
+	public List<Perturbation> getRandomPerturbations(int numberOfPerturbations) {
+		
+		PriorityQueue<Perturbation> perturbations = new PriorityQueue<Perturbation>();
+		List<Perturbation> randomPerturbations = new ArrayList<Perturbation>(numberOfPerturbations);
+		List<Integer> initialPerturbationVector = Arrays.asList(Integer.valueOf(1));
+		Perturbation initialPerturbation = new Perturbation(initialPerturbationVector, getPerturbationScore(initialPerturbationVector)), candidatePerturbation = null;
+		perturbations.add(initialPerturbation);
+		
+		for (int perturbationCounter = 0; perturbationCounter < numberOfPerturbations; ++perturbationCounter) {
+			
+			do {
+				
+				candidatePerturbation = perturbations.remove();
+				perturbations.add(shiftPerturbation(candidatePerturbation));
+				perturbations.add(expandPerturbation(candidatePerturbation));
+			
+			} while(!isValidPerturbation(candidatePerturbation));
+			
+			randomPerturbations.add(candidatePerturbation);
+		}
+		
+		return randomPerturbations;
 	}
 
 }
---
+++ lsh/querying/Perturbation.java
@@ -0,0 +1,32 @@
+package lsh.querying;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Perturbation implements Comparable<Perturbation> {
+	
+	private List<Integer> perturbedVector;
+	private double perturbationScore;
+	
+	public Perturbation(List<Integer> perturbedVector, double perturbationScore) {
+		this.perturbedVector = perturbedVector;
+		this.perturbationScore = perturbationScore;
+	}
+
+	@Override
+	public int compareTo(Perturbation other) {
+		
+		if (other == null) {
+			throw new NullPointerException();
+		}
+		
+		Perturbation otherPerturbation = (Perturbation) other;
+		return Double.compare(Double.valueOf(this.perturbationScore), Double.valueOf(otherPerturbation.perturbationScore));
+
+	}
+
+	public List<Integer> getPerturbedVector() {
+		return Collections.unmodifiableList(this.perturbedVector);
+	}
+
+}

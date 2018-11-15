89cf76755420cfc89a51ccb328240d8e0338a8fe
Ben Nelson <nelson.ben.c@gmail.com>
2018-11-15 00:03:34 -0700

    WIP: add image to feature histogram

---
+++ lsh/images/FeatureFactory.java
@@ -0,0 +1,13 @@
+package lsh.images;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.media.jai.Histogram;
+
+public class FeatureFactory implements Serializable {
+
+    public void createHistogramOfImage(String path) {
+
+    }
+}

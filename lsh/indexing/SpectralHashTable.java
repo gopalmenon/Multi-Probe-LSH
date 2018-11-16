9c7e52d53002eea9b5bf8aa14bc766edafeac1e7
Corbin <corbinwbaldwin@gmail.com>
2018-11-15 17:08:52 -0700

    Eliminate unnecessary import

--- lsh/indexing/SpectralHashTable.java
+++ lsh/indexing/SpectralHashTable.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.apache.commons.math3;
-import lsh.indexing.HashTable;
 
 public class SpectralHashTable extends HashTable{
 

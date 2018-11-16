f54ea1732d0f9a68706773a6fdebbf4245eda12d
Corbin <corbinwbaldwin@gmail.com>
2018-11-15 22:04:44 -0700

    Add working spectral constructors

--- lsh/indexing/HashTable.java
+++ lsh/indexing/HashTable.java
@@ -1,5 +1,9 @@
 package lsh.indexing;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.EigenDecomposition;
+import org.apache.commons.math3.linear.RealMatrix;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,7 +32,42 @@ public class HashTable implements Serializable {
 		this.numberOfHashFunctions = numberOfHashFunctions;
 		this.objectIndex = new HashMap<HashBucket, SearchableObject>();
 	}
-	
+
+	public HashTable(int numberOfHashFunctions, int numberOfDimensions, List<SearchableObject> featureVectors) {
+		this.hashFunctionTable = new ArrayList<HashFunction>(numberOfHashFunctions);
+
+		// Restrict the number of hash functions to be min(numberOfDimensions, numberOfHashFunctions)
+		this.numberOfHashFunctions = (numberOfHashFunctions <= numberOfDimensions) ? numberOfHashFunctions : numberOfDimensions;
+
+		// Begin construction of matrix M
+		double M[][] = new double[featureVectors.size()][numberOfDimensions];
+
+		for (int point_ndx = 0; point_ndx < featureVectors.size(); point_ndx++) {
+			// Set rows of M to be featurevectors
+			double featureVector[] = new double[numberOfDimensions];
+
+            // Perhaps there is a better way to do this?
+			for (int i = 0; i < numberOfDimensions; i++)
+				featureVector[i] = featureVectors.get(point_ndx).getObjectFeatures().get(i);
+
+			M[point_ndx] = featureVector;
+		}
+
+		// Get Eigendecomposition
+		RealMatrix realM = new Array2DRowRealMatrix(M);
+		RealMatrix MtM = (realM.transpose()).multiply(realM);
+		EigenDecomposition VDV = new EigenDecomposition(MtM);
+		RealMatrix V = VDV.getV();
+
+		for (int hashFunctionCounter = 0; hashFunctionCounter < this.numberOfHashFunctions; ++hashFunctionCounter) {
+			double eigenVector[] = V.getColumn(hashFunctionCounter);
+
+			this.hashFunctionTable.add(new HashFunction(numberOfDimensions, 0.0, eigenVector));
+		}
+
+		this.objectIndex = new HashMap<HashBucket, SearchableObject>();
+	}
+
 	/**
 	 * @param searchableObject
 	 * @return the bucket corresponding to the object
